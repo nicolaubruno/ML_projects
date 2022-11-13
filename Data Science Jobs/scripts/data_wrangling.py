dcfe5a44459d57590db5f69b9fa97b073cf065a8
Bruno Santos <nicolau.bruno@gmail.com>
2022-11-13 12:59:53 -0300

    Update the structure of the Data Science Jobs project

---
+++ Data Science Jobs/scripts/data_wrangling.py
@@ -0,0 +1,64 @@
+#
+# Libraries
+#
+
+import numpy as np
+import pandas as pd
+
+from IPython.display import display_html
+
+from sklearn.preprocessing import StandardScaler
+
+#
+# Functions
+#
+
+def read_dataset(filepath):
+    """
+        Read and clean CSV dataset from the jobseeker website ai-jobs
+        https://ai-jobs.net/
+    """
+
+    # Getting dataframe (df) from the dataset
+    df = pd.read_csv(filepath)
+
+    # Getting only the relevant columns
+    df = df[['job_title', 'experience_level']].sort_values('job_title')
+
+    # Function to count a job vacancy in a specific experience level
+    def count_exp_level(df, level):
+        df = df.value_counts()
+
+        if level in df.index: return df[level]
+        else: return 0
+
+    # Aggregate over job titles considering experience levels
+    df = df.groupby('job_title').agg(
+        EN = ('experience_level', lambda x: count_exp_level(x, 'EN')),
+        MI = ('experience_level', lambda x: count_exp_level(x, 'MI')),
+        SE = ('experience_level', lambda x: count_exp_level(x, 'SE'))
+    )
+
+    # Select jobs with at least 10 vacancies
+    df = df.where(df.sum(axis = 1) >= 10).dropna().astype('int64')
+
+    # Scale data
+    scaled_df = pd.DataFrame(
+            StandardScaler().fit_transform(df),
+            index = df.index,
+            columns = df.columns
+        )
+
+    return df, scaled_df
+
+def display_side_by_side(*args):
+    """
+        Display dataframes side-by-side
+    """
+
+    html_str = ''
+
+    for df in args:
+        html_str += df.to_html()
+
+    display_html(html_str.replace('table','table style="display:inline; padding-right: 20px"'), raw=True)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ML_projects
-Projects to study Machine Learning methods.
+# Machine Learning Projects
+
+In this repository, we store projects to study and leverage machine learning methods. Until November 13th, 2022, this repository houses the following projects:
+
+- **Data Science Jobs**: In this project, we explore **clustering** methods by using this [dataset](https://salaries.ai-jobs.net/download/) from the jobseeker website [ai-jobs](https://ai-jobs.net/).
